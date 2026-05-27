f283c03eb3f87ee03cfd9cee4264b1e5c96c617d
snehaldighore21 <snehaldighore21@gmail.com>
2026-05-27 14:07:15 +0530

    Configure S3 backend with bucket and encryption

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,3 +1,8 @@
 terraform {
-  backend "s3" {}
-}
+  backend "s3" {
+            bucket         = "terraform-s3-tfbucket"
+            key            = "dev/terraform.tfstate"
+            region         = "ap-southeast-2"
+            encrypt        = true
+         }
+     }
